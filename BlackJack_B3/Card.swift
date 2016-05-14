9370a1e82ac6c75c81aaaffc57c76f46fc69d471
TheGerbille <gerbilledu0138@gmail.com>
2016-05-14 17:01:39 +0200

    Ajout du lien vers l'image de la carte

--- BlackJack_B3/Card.swift
+++ BlackJack_B3/Card.swift
@@ -27,6 +27,8 @@ class Card
     let value: Int;
     //Valeur réel de la carte
     let realValue: Int;
+    //Image de la carte
+    let image: String;
     
     //True si la carte est visible des joueurs (retournée)
     var visible: Bool;
@@ -37,6 +39,51 @@ class Card
         self.value = value;
         self.visible = true;
         
+        //Image selector:
+        var colorCode:String;
+        var numberCode:String;
+        switch color
+        {
+        case .Treffle:
+            colorCode = "C";
+        case .Coeur:
+            colorCode = "H";
+        case .Carreau:
+            colorCode = "D";
+        case .Pic:
+            colorCode = "S";
+        default:
+            colorCode = color.rawValue;
+        }
+        switch value {
+        case 1:
+            numberCode = "A";
+        case 10:
+            numberCode = "0";
+        case 11:
+            numberCode = "J";
+        case 12:
+            numberCode = "Q";
+        case 13:
+            numberCode = "K";
+        default:
+            numberCode = String(value);
+        }
+        if(color != .Bleu || color != .Rouge){
+            self.image = "http://deckofcardsapi.com/static/img/"+numberCode+colorCode+".png";
+        }else{
+            switch color {
+            case .Bleu:
+                self.image = "http://usercontent2.hubimg.com/6089769_f260.jpg";
+            case .Rouge:
+                self.image = "https://s3.amazonaws.com/ksr/assets/001/640/275/85ae4e674833c0b9454f6e9e651f94ff_large.jpg?1392530632";
+            default:
+                print("Error IMG initialisation");
+                self.image = "";
+            }
+        }
+        
+        //Real Value selector
         switch(value)
         {
             // Du 10 au Roi, la carte est valorisée à 10 points
